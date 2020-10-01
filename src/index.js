61135b92cb21a1f2ac3e48ea0a000d0d22797888
LevanovaElena <ellevanova@gmail.com>
2020-10-01 23:55:36 +0400

    feat:add function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array) return 0;
+  if (array.length === 0) return 0;
+  return array.sort((a, b) => a - b)[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array) return 0;
+  if (array.length === 0) return 0;
+  return array.sort((a, b) => a - b)[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array) return 0;
+  if (array.length === 0) return 0;
+  return array.reduce((sum, current) => sum + current, 0) / array.length;
 }
